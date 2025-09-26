30db9e37a181c7c4f3c003756d9eadacb1ceeecc
Leoces Martinez <leocesvives@gmail.com>
2025-09-26 07:07:36 -0500

    merge into feature/Leoces

---
+++ src/app/carrera/carrera.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrera',
+  imports: [],
+  templateUrl: './carrera.component.html',
+  styleUrl: './carrera.component.css'
+})
+export class CarreraComponent {
+
+}
